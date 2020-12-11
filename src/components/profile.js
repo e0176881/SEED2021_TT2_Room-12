094a78b4cdd31093a6a381126d1feab0f092e6ba
alexiahsu <hsu.enyang@gmail.com>
2020-12-11 14:54:17 +0800

    added placeholder for profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 function profile() {
-  return <div></div>;
+  return <div class="section-title">
+    <div class="container">
+      <div class="section-title">
+        <h2>About you</h2>
+        <p>We love to know more about you so that we make sure you get nothing but the best from us. Do update your details with us if they aren't correct.</p>
+      </div>
+      <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4>Full Name</h4>
+              <p>insert da name</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4>NRIC</h4>
+              <p>NRIC</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4>Phone Number</h4>
+              <p>Number</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>;
 }
 
 export default profile;
