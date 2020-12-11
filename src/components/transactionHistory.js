1ac5be80d566c481b71a875f4c804363cedae6b5
vanessa <vanessaleofs@gmail.com>
2020-12-11 14:40:58 +0800

    transachist

--- src/components/transactionHistory.js
+++ src/components/transactionHistory.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function transactionHistory() {
-  return <div></div>;
+  return (
+  <div class="transactwrapper">
+      <h1 class="transactheader">Transaction History</h1>
+
+      <table class="transacttable">
+         <tr>
+         <td>date and time</td>
+         <td>payee name</td>
+         <td>amount</td>
+         </tr>
+      </table>
+
+  </div>);
 }
 
 export default transactionHistory;
