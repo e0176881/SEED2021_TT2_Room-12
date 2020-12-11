ea85eb256135f8e11a2bde0fbb298942b2128b78
Orson <e0176881@u.nus.edu>
2020-12-11 11:40:15 +0800

    api calling for login

--- server.js
+++ server.js
@@ -17,6 +17,29 @@ if (process.env.NODE_ENV != "dev") {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post("/api/login", (req, res) => {
+  axios
+    .post(
+      "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login",
+      {
+        username: "Group12",
+        password: "wuXznjuC4RADkOM",
+      },
+      {
+        headers: {
+          "x-api-key": "LOWUmvIAYg4sS1mrgJvxk6M1KHJbCQLs8T5SLyof",
+        },
+      }
+    )
+    .then((response) => response)
+    .catch((error) => error.response)
+    .then((response) => {
+      try {
+        res.status(response.status).send(response.data);
+      } catch (e) {}
+    });
+});
+
 app.get("/api/zz", (req, res) => {
   console.log("api/users called!!!!");
   res.send("ok");
