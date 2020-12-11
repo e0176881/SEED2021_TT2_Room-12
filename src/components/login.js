ff5dd76f34d7bda8685dabb381e42bea9c9643ef
vanessa <vanessaleofs@gmail.com>
2020-12-11 15:24:21 +0800

    login page

--- src/components/login.js
+++ src/components/login.js
@@ -73,7 +73,8 @@ export default class Login extends Component {
       <form onSubmit={this.login}>
         <div class="row mt-5 justify-content-center">
           <div class="col-lg-4">
-            <h3>Log in</h3>
+            <h3>Welcome to PayLLor!
+            </h3>
 
             <div className="form-group">
               
@@ -99,7 +100,9 @@ export default class Login extends Component {
               />
             </div>
           </div>
-
+          </div>
+          
+          <div class="row mt-5 justify-content-center">
           <div className="form-group">
             <div className="custom-control custom-checkbox">
               <input
@@ -115,9 +118,14 @@ export default class Login extends Component {
               </label>
             </div>
           </div>
+         
+          <div class="submitbtn">
           <button type="submit" className="btn btn-dark btn-lg btn-block">
             Sign in
           </button>
+          
+          </div>
+        
         </div>
       </form>
     );
