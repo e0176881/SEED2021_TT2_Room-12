464a797f2ce15af00f176d6d63dd00f075e8b884
vanessa <vanessaleofs@gmail.com>
2020-12-11 14:12:49 +0800

    adding

--- src/components/home.js
+++ src/components/home.js
@@ -34,41 +34,29 @@ export default class Home extends Component {
             <div class="row">
               <div class="col-lg-6">
                 <h1>Welcome {this.state.username}</h1>
-              </div>
-              <div class="col-lg-6 pt-4 pt-lg-0">
-                <h3>About Us</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <ul>
-                  <li>
-                    <i class="bx bx-check-double"></i> Ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat.
-                  </li>
-                  <li>
-                    <i class="bx bx-check-double"></i> Duis aute irure dolor in
-                    reprehenderit in voluptate velit.
-                  </li>
-                </ul>
-                <div class="row icon-boxes">
-                  <div class="col-md-6">
-                    <i class="bx bx-receipt"></i>
-                    <h4>Corporis voluptates sit</h4>
-                    <p>
-                      Consequuntur sunt aut quasi enim aliquam quae harum
-                      pariatur laboris nisi ut aliquip
-                    </p>
+                <div class="row">
+                  <div class="accountbalance">
+                        insert account balance here 
                   </div>
-                  <div class="col-md-6 mt-4 mt-md-0">
-                    <i class="bx bx-cube-alt"></i>
-                    <h4>Ullamco laboris nisi</h4>
-                    <p>
-                      Excepteur sint occaecat cupidatat non proident, sunt in
-                      culpa qui officia deserunt
-                    </p>
+
+                  <div class="transactionhist">
+                        <table class="transactionhist">
+                          <tr>
+                            <td align="left">payee name</td>
+                            <td align="right">amount</td>
+                          </tr>
+                        </table>
                   </div>
                 </div>
+
+              </div>
+              <div class="col-lg-6 pt-4 pt-lg-0">
+                <h3>Functions here</h3>
+                <p>
+                      
+                </p>
+                
+                
               </div>
             </div>
           </div>
