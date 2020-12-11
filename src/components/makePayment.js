b19532ddae7f3c47c86f15d929cecfd55226ff5b
Pamela <e0202709@u.nus.edu>
2020-12-11 15:33:44 +0800

    halfway

--- src/components/makePayment.js
+++ src/components/makePayment.js
@@ -1,42 +1,111 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useEffect, useState, Redirect } from "react";
+import axios from "axios";
+function MakePayment() {
+  const { register, handleSubmit } = useForm();
+  const [custId, setCustId] = useState("");
 
-function makePayment() {
-  return <div class="section-title">
+  const onSubmit = (data) => {
+    setCustId(JSON.parse(localStorage.getItem("userLoginDetails")).custID);
+    var date = new Date();
+    axios
+      .post("/api/addtransaction", {
+        custID: custId,
+        payeeID: 1,
+        dateTime: date,
+        amount: data.amount,
+        expensesCat: data.expCat,
+        eGift: data.eGift,
+        message: data.message,
+      })
+      .then(function (response) {
+        console.log(response.data);
+
+        // console.log("test" + { token });
+
+        // setRedirect(true);
+
+        //return <Redirect to="./shop" />;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div class="section-title">
       <div class="container">
-      <div class="row">
-            <div class="col-md-6">
-              <div class="icon-box">
-                <h4>Ready to make quick payments?</h4>
-                <p>Don't worry, we've got you! Make fast and secure payments to anyone now!</p>
-              </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="icon-box">
+              <h4>Ready to make quick payments?</h4>
+              <p>
+                Don't worry, we've got you! Make fast and secure payments to
+                anyone now!
+              </p>
             </div>
-            <div class="col-md-6 mt-4 mt-lg-0">
-              <div class="icon-box">
-                <form>
-                  <label>Payee</label>
-                  <input
-                    type="text"
-                    name="payeename"
-                    className="form-control"
-                    placeholder="Enter Payee Name"
-                  />
-                  <label>Amount</label>
-                  <input
-                    type="float"
-                    name="payamount"
-                    className="form-control"
-                    placeholder="Enter Amount"
-                  />
-                  <br></br>
-                  <button type="submit" className="btn btn-dark btn-lg btn-block">
-                    Pay lor!
-                  </button>
-                </form>
-              </div>
+          </div>
+          <div class="col-md-6 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Payee's NRIC</label>
+                <input
+                  type="text"
+                  name="payeenric"
+                  className="form-control"
+                  placeholder="Enter Payee Name"
+                />
+                <label>Amount</label>
+                <input
+                  type="float"
+                  name="amount"
+                  className="form-control"
+                  placeholder="Enter Amount"
+                />
+                <label>Expense Category</label>
+                <input
+                  type="text"
+                  name="expCat"
+                  className="form-control"
+                  placeholder="Enter Expense Category"
+                />
+                <div className="form-group">
+                  <div className="custom-control custom-checkbox">
+                    <input
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="customCheck1"
+                      name="eGift"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customCheck1"
+                    >
+                      EGift
+                    </label>
+                  </div>
+                </div>
+
+                <label>Message</label>
+                <input
+                  type="text"
+                  name="message"
+                  className="form-control"
+                  placeholder="Enter Message"
+                />
+
+                <br></br>
+                <button type="submit" className="btn btn-dark btn-lg btn-block">
+                  Pay lor!
+                </button>
+              </form>
             </div>
+          </div>
         </div>
       </div>
-    </div>;
+    </div>
+  );
 }
 
-export default makePayment;
+export default MakePayment;
