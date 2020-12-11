fefb90281ff294e71e2c60db60053c6551b962a4
alexiahsu <hsu.enyang@gmail.com>
2020-12-11 13:15:43 +0800

    added card and column

--- src/components/home.js
+++ src/components/home.js
@@ -24,47 +24,39 @@ export default class Home extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome (insert name code)</h1>
-        <div class="card">
+        <section>
+        <div class="container">
+
           <div class="row">
-            <div class="col-sm-6 col-lg-4">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Balance
-                </a>
-              </div>
+            <div class="col-lg-6">
+              <h1>Welcome message here</h1>
             </div>
-            <div class="col-sm-6 col-lg-4">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Transfer link
-                </a>
-              </div>
-            </div>
-            <div class="col-sm-6 col-lg-4">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Transaction history
-                </a>
+            <div class="col-lg-6 pt-4 pt-lg-0">
+              <h3>About Us</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <ul>
+                <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              </ul>
+              <div class="row icon-boxes">
+                <div class="col-md-6">
+                  <i class="bx bx-receipt"></i>
+                  <h4>Corporis voluptates sit</h4>
+                  <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                </div>
+                <div class="col-md-6 mt-4 mt-md-0">
+                  <i class="bx bx-cube-alt"></i>
+                  <h4>Ullamco laboris nisi</h4>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+
+          </div>
+        </section>
       </div>
     );
   }
